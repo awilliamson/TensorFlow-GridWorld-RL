927c307ac40a1832c749e7795c14b77107b5abc0
Ashley Williamson <11356993@students.lincoln.ac.uk>
2016-12-08 10:29:55 +0000

    Serialise GridWorld function for DQN input_shape

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from gridworld import *
 from dqn import *
 
+def serialise_gridworld( gw ):
+    rtn = []
+    for x in range(gw.height):
+        for y in range(gw.width):
+            if gw.player == (y, x):
+                rtn.append('P')  # Visualise where the player is
+            else:
+                rtn.append(gw.map[y][x])  # Flip columns and rows for pretty-printing
+    return rtn
+
 if __name__ == "__main__":
     gw = GridWorld()
-    dqn = DQN()
+    dqn = DQN(input_shape=[None, len(serialise_gridworld(gw)), 1], num_outputs=4)
 
     gw = GridWorld(5, 5)
     print(gw)
@@ -36,4 +46,5 @@ if __name__ == "__main__":
     print(gw.getReward())
     print(gw)
 
+    assert (gw.player == gw.goal)
     print(gw.player, gw.goal)
