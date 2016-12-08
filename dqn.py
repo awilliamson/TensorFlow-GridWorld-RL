6a00de784c59074706d9389e79840951e0caed12
Ashley Williamson <11356993@students.lincoln.ac.uk>
2016-12-08 10:29:36 +0000

    Session support, target_q and q_network variables

--- dqn.py
+++ dqn.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
 import tflearn
+from collections import deque
 
 class DQN():
-    def __init__(self, input_shape=None, num_outputs=4):
+    def __init__(self, sess=None, input_shape=None, num_outputs=4):
 
         # Cannot use mutable type for default, as this does not create a new shape every invocation, but only once
         # for all instances. Therefore we must set the default from within, and use a Sentinel value to denote
@@ -10,9 +11,27 @@ class DQN():
         if input_shape is None:
             input_shape = [None, 5, 5, 1]
 
+        self.s = sess if sess is not None else tf.Session()
+
         """Placeholder"""
         self.input_shape = input_shape
         self.num_outputs = num_outputs
+
+        self.experience_replay_size = 500
+        # Create experience replay to a maximum size N.
+        self.experience_replay = deque(maxlen=self.experience_replay_size)
+
+        # Used for updating target Q Network's Weights.
+        self.target_q_network_update_frequency = 100
+        self.target_q_network_update_coefficient = 0.01 # How much to update Target Q weights, based on loss.
+
+        # Initialise action-value Q function
+        self.q_network = self.create_model()
+        # Initialise target action-value Q function; Weights will be frozen, and only periodically updated.
+        self.target_q_network = self.create_model()
+
+        # Initialise all tf variables.
+        self.s.run(tf.initialize_all_variables())
 
     def create_model(self):
         net = tflearn.input_data( shape=self.input_shape, name="input")
